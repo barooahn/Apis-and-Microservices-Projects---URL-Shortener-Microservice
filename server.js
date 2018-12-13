34e7472334ea5a8d44668fdaff01b87f075b1b13
Glitch (ritzy-side) <none>
2018-12-13 17:09:58 +0000

    🔪🏜 Checkpoint ./server.js:2505047/590

--- server.js
+++ server.js
@@ -3,6 +3,8 @@
 var express = require('express');
 var mongo = require('mongodb');
 var mongoose = require('mongoose');
+var bodyParser = require('body-parser');
+const dns = require('dns');
 
 var cors = require('cors');
 
@@ -18,7 +20,7 @@ app.use(cors());
 
 /** this project needs to parse POST bodies **/
 // you should mount the body-parser here
-
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use('/public', express.static(process.cwd() + '/public'));
 
 app.get('/', function(req, res){
@@ -26,10 +28,13 @@ app.get('/', function(req, res){
 });
 
   
-// your first API endpoint... 
-app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
-});
+app.post('/api/shorturl/new', (req,res) => {
+  const url = req.body.url;
+  
+  dns.lookup(host, cb)
+  
+  res.json({ 'url': `${url}`});
+}); 
 
 
 app.listen(port, function () {
