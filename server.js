c812f2a4e84f9b7464c3a48773bcd6eea110ddc3
Glitch (ritzy-side) <none>
2018-12-14 13:51:06 +0000

    📻⛄️ Checkpoint ./server.js:2505047/1

--- server.js
+++ server.js
@@ -22,6 +22,7 @@ app.use(cors());
 /** this project needs to parse POST bodies **/
 // you should mount the body-parser here
 app.use(bodyParser.urlencoded({ extended: false }));
+
 app.use('/public', express.static(process.cwd() + '/public'));
 
 app.get('/', function(req, res){
